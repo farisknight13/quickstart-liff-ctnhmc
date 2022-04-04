fce60e2ace93d1f892e170edf156a92bfa2fb216
farisknight13 <knightfaris13@gmail.com>
2022-04-04 11:47:34 +0700

    2 files modified

--- index.js
+++ index.js
@@ -26,7 +26,21 @@ const friendShip = document.getElementById("friendShip")
 
 async function main() {
   // Initialize LIFF app)
-  
+  await liff.init({ liffId: "1657028029-QPkE5KoG" })
   // Try a LIFF function
+  switch (liff.getOS()) {
+    case "android": body.style.backgroundColor = "#d1f5d3"; break
+    case "ios": body.style.backgroundColor = "#eeeeee"; break
+  }
+
+  getUserProfile()
 }
 main()
+
+async function getUserProfile() {
+  const profile = await liff.getProfile()
+  pictureUrl.src = profile.pictureUrl
+  userId.innerHTML = "<b>userId:</b> " + profile.userId
+  statusMessage.innerHTML = "<b>statusMessage:</b> " + profile.statusMessage
+  displayName.innerHTML = "<b>displayName:</b> " + profile.displayName
+}
